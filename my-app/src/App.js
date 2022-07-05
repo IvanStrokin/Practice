2694fa9c8042a8ed636009f464ecee29e02b7c16
Tuniya4ec <arsenji.butuzov@gmail.com>
2022-07-05 20:18:57 +0400

    обнова

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,6 +21,7 @@ function App() {
         </a>
       </header>
       <Component/>
+      <Array/>
     </div>
   );
 }
--- my-app/src/Array/Array.jsx
+++ my-app/src/Array/Array.jsx
@@ -1,10 +1,7 @@
-import React from "react";
-
-const Array_Func = ({ }) => {
- const Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
- return Array.map((Array) => <li>{Array}</li>)
- return (<div>
-
-  </div>)
+import React from 'react'
+const Array_Func = ({}) => {
+  let Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let NewArray = Array.map((arr) => { return arr });
+ // return (<div>Babushka</div>)
 }
 export default Array_Func;
