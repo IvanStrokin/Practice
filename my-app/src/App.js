b3bc9d83e928931df8cdee48828fe18219fabfa4
StrokinIvan <strokinIv23@yandex.ru>
2022-07-06 12:43:56 +0400

    button1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Component from './Component';
 import Array from './Array';
@@ -6,21 +5,8 @@ import Array from './Array';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Component/>
+      <Array/>
     </div>
   );
 }
--- my-app/src/Array/Array.jsx
+++ my-app/src/Array/Array.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import Button1
+  from "../Buttons/Button1";
 const Array_Func = ({ }) => {
- const Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
- return Array.map((Array) => <li>{Array}</li>)
- return (<div>
+  const Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  </div>)
+  const a = Array.map((item) => <li>{item}</li>)
+  return (
+    <>
+      <ul>
+        {a}
+      </ul>
+      {/* Удалить первый элемент массива
+          Удалить последний элемент массива
+          Добавить элемент в начало массива
+          Добавиьт элемент в конец массива
+          Вывести в обратном порядке
+       */}
+      <Button1 oldArrey={Array}/>
+    </>
+  )
 }
 export default Array_Func;
