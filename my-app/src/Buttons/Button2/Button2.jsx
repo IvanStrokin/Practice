67828eee40f0ebea909db135235704b7d663a28c
Tuniya4ec <arsenji.butuzov@gmail.com>
2022-07-10 11:31:07 +0400

    Merge branch 'main' of https://github.com/IvanStrokin/Practice

--- my-app/src/Buttons/Button2/Button2.jsx
+++ my-app/src/Buttons/Button2/Button2.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Button1 = ({ oldArrey }) => {
+const Button2 = ({ oldArrey }) => {
   const Start_Addition = () => {
     const new_arry = oldArrey.unshift(4); //????
     alert(new_arry);
@@ -9,9 +9,9 @@ const Button1 = ({ oldArrey }) => {
   return (
     <button
       onClick={() => { Start_Addition() }}>
-      "Добавить новый первый элемент массива"
+      "Удалить первый элемент массива"
     </button>
   )
 }
 
-export default Button1;
+export default Button2;
